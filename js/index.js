d1c57a4e1edc1788ebb86098e8f92a545397d647
Jiechang Wang <249280082@qq.com>
2017-07-25 21:31:13 +0800

    edit demo

--- js/index.js
+++ js/index.js
@@ -2,7 +2,5 @@ var imageWrapper = document.getElementsByClassName('image-wrapper');
 var image = document.getElementsByClassName('image');
 var wrapperWidth = imageWrapper[0].offsetWidth;
 var wrapperHeight = imageWrapper[0].offsetHeight;
-var adaptationType = 1;
-Array.from(image).forEach(function(element) {
-  renderSelfAdaptationImage(element, adaptationType, wrapperWidth, wrapperHeight);
-});
+renderSelfAdaptationImage(image[0], 1, wrapperWidth, wrapperHeight);
+renderSelfAdaptationImage(image[1], 2, wrapperWidth, wrapperHeight);
